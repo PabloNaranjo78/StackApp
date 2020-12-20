e01ca209b684bd789f0d5dc93da04c8acb8a1d9f
PabloNaranjo78 <67924944+PabloNaranjo78@users.noreply.github.com>
2020-12-20 01:43:29 -0600

    Se hace la interfaz y se prueban botones

--- app/src/main/java/cr/ac/tec/stackapp/MainActivity.java
+++ app/src/main/java/cr/ac/tec/stackapp/MainActivity.java
@@ -1,6 +1,9 @@
 package cr.ac.tec.stackapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,42 +15,26 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView numView;
+    EditText entry;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        this.numView = findViewById(R.id.numView);
+        this.entry = findViewById(R.id.entry);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void push(View view){
+        this.numView.setText("Prueba");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void pop(View view){
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void peek(View view){
+
     }
 }
